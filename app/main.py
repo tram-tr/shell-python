a84543877a208ed758bd4dc21a7ace393e1b2893
tram-tr <htrinh@nd.edu>
2026-01-03 22:38:45 -0600

    implement type

--- app/main.py
+++ app/main.py
@@ -9,7 +9,22 @@ def main():
         if cmd == "exit":
             break
         elif cmd == "echo":
+            if len(args) == 0:
+                print()
             print(" ".join(args))
+        elif cmd == "type":
+            if len(args) != 1:
+                print("type: usage: type COMMAND")
+            else:
+                command = args[0]
+                if command == "echo":
+                    print(f"{command} is a shell builtin")
+                elif command == "type":
+                    print(f"{command} is a shell builtin")
+                elif command == "exit":
+                    print(f"{command} is a shell builtin")
+                else:
+                    print(f"{command}: not found")
         else:
             print(f"{cmd}: command not found")
 
