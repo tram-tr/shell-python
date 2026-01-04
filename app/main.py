299c39d5cbd26d171f87727681c01f819e6d76cb
tram-tr <htrinh@nd.edu>
2026-01-03 22:49:27 -0600

    locate executable files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import sys
-
+import os
 
 def main():
+    shell_cmds = ["echo", "type", "exit"]
     while True:
         sys.stdout.write("$ ")
         str = input()
@@ -17,17 +18,28 @@ def main():
                 print("type: usage: type COMMAND")
             else:
                 command = args[0]
-                if command == "echo":
-                    print(f"{command} is a shell builtin")
-                elif command == "type":
-                    print(f"{command} is a shell builtin")
-                elif command == "exit":
+                if command in shell_cmds:
                     print(f"{command} is a shell builtin")
                 else:
-                    print(f"{command}: not found")
+                    found = False
+                    path_env = os.environ.get("PATH", "")
+                    # go through each directory in PATH
+                    for dir in path_env.split(os.pathsep):
+                        if dir == "":
+                            dir = "."
+                        full_path = os.path.join(dir, command) 
+                        # check if the file exists and is executable
+                        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                            found = True
+                            break
+                    if found:
+                        print(f"{command} is {full_path}")
+                    else:
+                        print(f"{command}: not found")
         else:
             print(f"{cmd}: command not found")
 
+    return 0
 
 if __name__ == "__main__":
     main()
